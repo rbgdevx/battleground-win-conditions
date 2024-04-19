19898dafcaf747b83eeaf79eb1c57d1d98e17f2d
RBG DEV <149433862+rbgdevx@users.noreply.github.com>
2024-04-18 23:00:02 -0400

    updating toc

--- core/config.lua
+++ core/config.lua
@@ -117,7 +117,7 @@ NS.userClassHexColor = "|c" .. select(4, GetClassColor(NS.userClass))
 
 NS.ADDON_PREFIX = "BGWC_VERSION"
 NS.FoundNewVersion = false
-NS.VERSION = 9320
+NS.VERSION = 9321
 
 NS.DefaultDatabase = {
   global = {
