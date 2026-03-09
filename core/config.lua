e189ff99c4d7911a4c47d775d3bb8730469c9766
RBGDEV <149433862+rbgdevx@users.noreply.github.com>
2026-03-09 01:13:36 -0700

    vers

--- core/config.lua
+++ core/config.lua
@@ -145,7 +145,7 @@ NS.userClassHexColor = "|c" .. select(4, GetClassColor(NS.userClass))
 
 NS.ADDON_PREFIX = "BGWC_VERSION"
 NS.FoundNewVersion = false
-NS.VERSION = 981
+NS.VERSION = 982
 
 NS.DefaultDatabase = {
   lastReadVersion = "9.8.1",
